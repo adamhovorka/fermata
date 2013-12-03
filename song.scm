e37c7fdcf9f5c71036aeb3f894ebd5f04a06c7f0
Adam Hovorka <adamhovorka@gmail.com>
2013-12-02 20:28:41 -0700

    Rearrangemént + Note lengths (sort of)

--- song.scm
+++ song.scm
@@ -1,7 +1,10 @@
 ;; Base Definitions
+(define tempo 120)
 (define resolution 256)
 (define sample-rate 48000)
 (define pi 3.141592653589793238)
+
+;;; {{{ Utilities
 
 ;; This is like the opposite of absolute value.
 ;; It makes the square wave a lot easier.
@@ -13,12 +16,53 @@
 ;; 'Cause the Scheme `modulo` function only works on integers...
 (define (% x m) (- x (* (floor (/ x m)) m)))
 
-;; TODO Note Lengths
-(define tempo 120)
-
 ;; The input to this function is the distance from A over middle C.
 (define (note->freq note)
         (* (expt 2 (/ note 12)) 440))
+
+;; Imposes a volume on instrument
+;; Volume is a decimal - e.g. 0.5 = half
+(define (vol instrument volume)
+        (lambda (pos) (* (instrument pos) volume)))
+
+;; No effect = identity function
+(define (no-effect x) x)
+
+;; Combines several notes or sequences into one
+(define (chord . notes)
+        (lambda (pos)
+                (/ (apply + (map
+                            (lambda (instrument) (instrument pos))
+                            notes))
+                   (length notes))))
+
+;;; }}}
+
+;;; {{{ Rendering functions
+
+;; Caps the output at the resolution so it doesn't b0rk everything else.
+(define (cap x)
+        (cond ((< x 0) 0)
+              ((>= x resolution) (- resolution 1))
+              (else x)))
+
+;; Justifies the output to the specified resolution.
+;; The input to this function should be in the range -1 < x < 1
+(define (justify x)
+        (let ((res (/ resolution 2)))
+             (cap (inexact->exact (floor
+                  (+ (* x res) res))))))
+
+;; Takes in a function and outputs the sampled hex values.
+(define (publish func len)
+	(define speed (/ tempo 60 4))
+	(set! len (/ len speed))
+        (let loop ((position 0))
+             (print (justify (func (/ position sample-rate))))
+             (if (< position (* sample-rate len))
+                 (loop (+ position 1)))))
+
+;;; }}}
 
 ;;; {{{ Basic instruments
 
@@ -55,28 +99,20 @@
 
 ;;; }}}
 
-;; Imposes a volume on instrument
-;; Volume is a decimal - e.g. 0.5 = half
-(define (vol instrument volume)
-        (lambda (pos) (* (instrument pos) volume)))
+;(define test-instrument
+        ;     waveform    A     D     S   R   effect
+       ;(list square-wave 0.125 0.125 0.5 0.2 no-effect))
 
 ;; Temporary notes
 ;; We need to find a better way to do this.
-(define b (sawtooth-wave (note->freq 2)))
-(define c (sawtooth-wave (note->freq 3)))
-(define d (sawtooth-wave (note->freq 5)))
-(define e (sawtooth-wave (note->freq 7)))
-(define f (sawtooth-wave (note->freq 9)))
-(define g (sawtooth-wave (note->freq 10)))
-(define a (sawtooth-wave (note->freq 12)))
-
-;; Combines several notes or sequences into one
-(define (chord . notes)
-        (lambda (pos)
-                (/ (apply + (map
-                            (lambda (instrument) (instrument pos))
-                            notes))
-                   (length notes))))
+(define waveform square-wave)
+(define b (waveform (note->freq 2)))
+(define c (waveform (note->freq 3)))
+(define d (waveform (note->freq 5)))
+(define e (waveform (note->freq 7)))
+(define f (waveform (note->freq 9)))
+(define g (waveform (note->freq 10)))
+(define a (waveform (note->freq 12)))
 
 ;; Test chord sequence
 (define I   (chord c e g))
@@ -84,28 +120,8 @@
 (define III (chord b d g))
 (define IV  (chord c e g))
 
-;; Caps the output at the resolution so it doesn't b0rk everything else.
-(define (cap x)
-        (cond ((< x 0) 0)
-              ((>= x resolution) (- resolution 1))
-              (else x)))
-
-;; Justifies the output to the specified resolution.
-;; The input to this function should be in the range -1 < x < 1
-(define (justify x)
-        (let ((res (/ resolution 2)))
-             (cap (inexact->exact (floor
-                  (+ (* x res) res))))))
-
-;; Takes in a function and outputs the sampled hex values.
-(define (publish func)
-        (let loop ((position 0))
-             (print (justify (func (/ position sample-rate))))
-             (if (< position (* sample-rate 1))
-                 (loop (+ position 1)))))
-
-(publish I)
-(publish II)
-(publish III)
-(publish IV)
+(publish I   0.25)
+(publish II  0.125)
+(publish III 0.125)
+(publish IV  0.5)
 (exit)
